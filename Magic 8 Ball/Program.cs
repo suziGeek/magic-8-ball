cea0116d3f2f1e90a3b341a26654c6e4df3aa569
scott balser <scottbalser@gmail.com>
2020-06-28 21:40:58 -0500

    Add project files.

---
+++ Magic 8 Ball/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Magic_8_Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                          88888888888                             ");
+            Console.WriteLine("                       88             88                          ");
+            Console.WriteLine("                     88                 88                        ");
+            Console.WriteLine("                    88       MAGIC       88                       ");
+            Console.WriteLine("                   88                     88                      ");
+            Console.WriteLine("                  88         ( 8 )         88                     ");
+            Console.WriteLine("                   88                     88                      ");
+            Console.WriteLine("                    88       BALL?       88                       ");
+            Console.WriteLine("                     88                 88                        ");
+            Console.WriteLine("                       88             88                          ");
+            Console.WriteLine("                          88888888888                             ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine("===                                                            ===");
+            Console.WriteLine("===                    Ask the MAGIC 8 Ball                    ===");           
+            Console.WriteLine("===                                                            ===");
+            Console.WriteLine("===                  Concentrate on the Question               ===");
+            Console.WriteLine("===              press the ENTER key for the answer            ===");
+            Console.WriteLine("===                                                            ===");
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+          // Console.ReadLine();
+
+            var ballArray = new string[10];
+                       
+            ballArray[0] = "            As I see it, yes."; 
+            ballArray[1] = "            It is certain.";
+            ballArray[2] = "            It is decidedly so.";
+            ballArray[3] = "            Signs point to yes.";
+            ballArray[4] = "            Don’t count on it.";
+            ballArray[5] = "            Outlook not so good.";
+            ballArray[6] = "            Very doubtful.";
+            ballArray[7] = "            Reply hazy, try again.";
+            ballArray[8] = "            Concentrate and ask again.";
+            ballArray[9] = "            Ask again later";
+
+            var rng = new Random();
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.ReadLine();
+                var randomize = rng.Next(0, ballArray.Length - 1);
+                Console.WriteLine();
+                Console.WriteLine("------------------------------------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine($"         { ballArray[randomize] }                    ");
+                Console.WriteLine();
+                Console.WriteLine("------------------------------------------------------------------");
+                
+            }
+
+            //  var randomize = rng.Next(0, ballArray.Length -1);
+
+            // Console.WriteLine($"{ ballArray[randomize] }");
+
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
